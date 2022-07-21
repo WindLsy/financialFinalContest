02f090034d3b6f24072325ca3221a10a90b2ea0a
linshiyou <2335404839@qq.com>
2022-07-21 17:31:26 +0800

    网关服务基础框架

--- financialFinalContest-gateway/src/main/java/cn/linshiyou/financialFinalContest/gateway/GateWayApplication.java
+++ financialFinalContest-gateway/src/main/java/cn/linshiyou/financialFinalContest/gateway/GateWayApplication.java
@@ -1,4 +1,4 @@
-package gateway;
+package cn.linshiyou.financialFinalContest.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
