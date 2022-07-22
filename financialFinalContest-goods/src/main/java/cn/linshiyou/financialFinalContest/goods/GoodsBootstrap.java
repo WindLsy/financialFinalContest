852207cb9668ebf6deeaaf286ce6a8656cb402e2
linshiyou <2335404839@qq.com>
2022-07-22 16:15:48 +0800

    物品服务-日常更新

---
+++ financialFinalContest-goods/src/main/java/cn/linshiyou/financialFinalContest/goods/GoodsBootstrap.java
@@ -0,0 +1,18 @@
+package cn.linshiyou.financialFinalContest.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: LJ
+ * @Description:
+ */
+@SpringBootApplication
+public class GoodsBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsBootstrap.class, args);
+    }
+
+
+}
